625c30b6858d680e3c9d07fd226f9ede548706e5
FlashL3opard <69573060+Flashl3opard@users.noreply.github.com>
2025-01-13 22:03:24 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navigation from './Components/Navigation';
+import Content from './Components/Content';
+const App = () => {
+    return <div>
+        <Navigation />
+        <Content />
+
+
+    </div>
+};
+
+export default App;
